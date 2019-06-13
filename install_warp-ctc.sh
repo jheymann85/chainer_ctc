a95eb3436a5001a00a836fe231083237a68b1145
jheymann85 <jahnheymann@gmail.com>
2019-06-13 05:56:56 +0200

    Update Git for warp-ctc to support newer GPUs
    See https://github.com/espnet/espnet/issues/733

--- install_warp-ctc.sh
+++ install_warp-ctc.sh
@@ -2,7 +2,9 @@
 
 mkdir -p ext/warp-ctc
 cd ext/warp-ctc
-git clone https://github.com/baidu-research/warp-ctc.git .
+# git clone https://github.com/baidu-research/warp-ctc.git .
+# Temporary fix for newer GPUs. See https://github.com/espnet/espnet/issues/733
+git clone https://github.com/jnishi/warp-ctc .
 mkdir build
 cd build
 cmake ../
